97dc6be9c06e828d854230873c107c2f7dc8a466
hezkore <hezkore@gmail.com>
2024-01-18 06:19:45 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,63 @@
+# Bash AI (bai)
+
+Bash AI (bai) is a bash shell script that acts as an AI assistant, allowing you to use natural language for any terminal-based task. It provides command suggestions based on your input and allows you to execute the suggested commands if desired.
+
+## Features
+
+- Natural language interface: Communicate with the terminal using everyday language.
+- Command suggestions: Get intelligent command suggestions based on your input.
+- Command information: Provides details about the suggested command.
+- Execute commands: Choose to execute the suggested commands directly from bai.
+
+## Installation
+
+1. Clone the repository:
+
+	```bash
+	git clone https://github.com/hezkore/bash-ai.git
+	```
+2. Make the script executable:
+
+	```bash
+	chmod +x bai.sh
+	```
+
+3. Execute bai:
+
+	```bash
+	./bai.sh
+	```
+
+## Configuration
+
+On the first run, a configuration file named `bai.cfg` will be created in your `~/.config` directory.\
+You must provide your OpenAI key in the `key=` field of this file. The OpenAI key can be obtained from your OpenAI account.
+
+> [!CAUTION]
+> Keeping the key in a plain text file is dangerous, and it is your responsibility to keep it secure.
+
+You can also change the model, temperature and query in this file.
+
+> [!TIP]
+> model `gpt-4` produces much better results than the standard `gpt-3.5-turbo` model.
+
+## Usage
+
+Run `./bai.sh your request` and Bash AI will return a suggestion for your request.\
+For example:
+
+```
+./bai.sh create a new directory with a name of your choice, then create a text file inside it
+```
+
+## Prerequisites
+
+- [OpenAI account and API key](https://platform.openai.com/apps)
+- [Curl](https://curl.se/download.html)
+- [JQ](https://stedolan.github.io/jq/download/)
+
+## License
+
+This project is licensed under the GNU General Public License v3.0 (GPLv3). This means you are free to use, modify, and distribute the original or modified content. However, if you modify the code, you are required to distribute your changes under the same license, thereby contributing your changes back to the community.
+
+For more information, please see the [GPLv3 FAQ](https://www.gnu.org/licenses/gpl-faq.html).
---
+++ bai.sh
@@ -0,0 +1,154 @@
+#!/bin/bash
+# -*- Mode: sh; coding: utf-8; indent-tabs-mode: t; tab-width: 4 -*-
+
+# Constants
+PRE_TEXT="  "
+NO_REPLY_TEXT="¯\_(ツ)_/¯"
+CMD_TEXT_COLOR="\e[48;5;236m\e[38;5;203m"
+INFO_TEXT_COLOR="\e[90;3m"
+CANCEL_TEXT_COLOR="\e[93m"
+OK_TEXT_COLOR="\e[92m"
+RESET_COLOR="\e[0m"
+CLEAR_LINE="\033[2K\r"
+DEFAULT_QUERY="Return a JSON object containing 'cmd' and 'info' fields. 'cmd' is the simplest POSIX Bash command for the query. 'info' provides details on what the command does."
+
+# Configuration file path
+CONFIG_FILE=~/.config/bai.cfg
+
+# Check for configuration file existence
+if [ ! -f "$CONFIG_FILE" ]; then
+	# Initialize configuration file with default values
+	echo "key=" >> "$CONFIG_FILE"
+	echo "" >> "$CONFIG_FILE"
+	echo "api=https://api.openai.com/v1/chat/completions" >> "$CONFIG_FILE"
+	echo "model=gpt-3.5-turbo" >> "$CONFIG_FILE"
+	echo "temp=0.1" >> "$CONFIG_FILE"
+	echo "tokens=100" >> "$CONFIG_FILE"
+	echo "query=" >> "$CONFIG_FILE"
+fi
+
+# Extract OpenAI Key from configuration
+config=$(cat "$CONFIG_FILE")
+
+# API Key
+OPENAI_KEY=$(echo "${config[@]}" | grep -oP '(?<=^key=).+')
+if [ -z "$OPENAI_KEY" ]; then
+	 # Prompt user to input OpenAI key if not found
+	echo "To use bai, please input your OpenAI key into the config file located at $CONFIG_FILE"
+	exit 1
+fi
+
+# Extract OpenAI URL from configuration
+OPENAI_URL=$(echo "${config[@]}" | grep -oP '(?<=^api=).+')
+
+# Extract OpenAI model from configuration
+OPENAI_MODEL=$(echo "${config[@]}" | grep -oP '(?<=^model=).+')
+
+# Extract OpenAI temperature from configuration
+OPENAI_TEMP=$(echo "${config[@]}" | grep -oP '(?<=^temp=).+')
+
+# Extract OpenAI system query from configuration
+OPENAI_QUERY=$(echo "${config[@]}" | grep -oP '(?<=^query=).+')
+
+# Extract maximum token count from configuration
+OPENAI_TOKENS=$(echo "${config[@]}" | grep -oP '(?<=^tokens=).+')
+
+# Apply default OpenAI system query if needed
+if [ -z "$OPENAI_QUERY" ]; then
+	OPENAI_QUERY="$DEFAULT_QUERY"
+fi
+
+# User AI query
+USER_QUERY=$@
+
+# Notify the user about our progress
+echo
+echo -ne "${PRE_TEXT}Thinking...\r"
+
+# Send request to OpenAI API
+RESPONSE=$(curl -s -X POST -H "Authorization:Bearer $OPENAI_KEY" -H "Content-Type:application/json" -d '{
+	"model": "'$OPENAI_MODEL'",
+	"max_tokens": '$OPENAI_TOKENS',
+	"messages": [
+		{
+			"role": "system",
+			"content": "'"${OPENAI_QUERY}"'"
+		},
+		{
+			"role": "user",
+			"content": "list all files"
+		},
+		{
+			"role": "assistant",
+			"content": "{ \"cmd\": \"ls -a\", \"info\": \"list all files, including hidden ones, in the current directory\" }"
+		},
+		{
+			"role": "user",
+			"content": "recursively list all the files"
+		},
+		{
+			"role": "assistant",
+			"content": "{ \"cmd\": \"ls -aR\", \"info\": \"list all files recursively, including hidden ones, in the current directory\" }"
+		},
+		{
+			"role": "user",
+			"content": "print hello world"
+		},
+		{
+			"role": "assistant",
+			"content": "{ \"cmd\": \"echo \\\"hello world\\\"\", \"info\": \"print the text \\\"hello world\\\" to the terminal\" }"
+		},
+		{
+			"role": "user",
+			"content": "'"${USER_QUERY}"'"
+		}
+	]
+}' $OPENAI_URL)
+
+# Extract the reply from the JSON response
+REPLY=$(echo "$RESPONSE" | jq -r '.choices[0].message.content' | sed "s/'//g")
+
+# Process the reply
+echo -ne "$CLEAR_LINE\r"
+if [ -z "$REPLY" ]; then
+	# We didn't get a reply
+	echo "${PRE_TEXT}${NO_REPLY_TEXT}"
+	exit 1
+else
+	# Extract command from response
+	CMD=$(echo "$REPLY" | jq -r '.cmd')
+	if [ -z "$CMD" ]; then
+		# If command is empty, print no reply text
+		echo
+		echo "${PRE_TEXT}${NO_REPLY_TEXT}"
+		exit 1
+	fi
+	
+	# Extract information from response
+	INFO=$(echo "$REPLY" | jq -r '.info')
+	if [ -z "$info" ]; then
+		info="warning: no information"
+	fi
+	
+	# Print command and information
+	echo -e "${PRE_TEXT}${CMD_TEXT_COLOR} ${CMD} ${RESET_COLOR}"
+	echo
+	echo -e "${PRE_TEXT}${INFO_TEXT_COLOR}${INFO}${RESET_COLOR}"
+	echo
+	
+	# Ask for user command confirmation
+	read -n 1 -p "${PRE_TEXT}execute command? [y/N]: " answer
+	echo;echo
+	if [ "$answer" == "Y" ] || [ "$answer" == "y" ]; then
+		eval "$CMD"
+		echo
+		# OK
+		echo -e "${OK_TEXT_COLOR}[ok]${RESET_COLOR}"
+	else
+		# CANCEL
+		echo -e "${CANCEL_TEXT_COLOR}[cancel]${RESET_COLOR}"
+	fi
+	echo
+	
+	exit 0
+fi
